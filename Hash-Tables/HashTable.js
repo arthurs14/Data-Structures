ee167868c18765c9b39165f1f2a1418d4f3bc3cb
arthurs14 <arthurcs426@gmail.com>
2020-05-18 15:31:49 -0700

    comments for further explanation on hash tables

--- Hash-Tables/HashTable.js
+++ Hash-Tables/HashTable.js
@@ -2,6 +2,7 @@
 // Mappings of key-value pairs
 // Extremely fast at insert, finding, and deleting values
 
+// turns key into a hashed value (numbered)
 const hash = (key, size) => {
   let hashedKey = 0;
 
@@ -17,16 +18,19 @@ class HashTable {
     this.size = 20;
     this.buckets = Array(this.size);
 
+    // creates a map object for each index in array
     for (let i = 0; i < this.buckets.length; i++) {
       this.buckets[i] = new Map();
     }
   }
 
+  // add key and value to the buckets
   insert = (key, value) => {
     let idx = hash(key, this.size);
     this.buckets[idx].set(key, value);
   }
 
+  // deletes value by finding the key in the buckets
   remove = (key) => {
     let idx = hash(key, this.size);
     let deleted = this.buckets[idx].get(key);
@@ -34,6 +38,7 @@ class HashTable {
     return deleted;
   }
 
+  // finds the key to return the value
   search = (key) => {
     let idx = hash(key, this.size);
     return this.buckets[idx].get(key);
