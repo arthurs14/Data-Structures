8983a8cdb259aa82a1d7d2a8736331f8b15b7acc
arthurs14 <arthurcs426@gmail.com>
2020-05-18 15:16:28 -0700

    hash table start

---
+++ Hash-Tables/HashTable.js
@@ -0,0 +1,50 @@
+// HASH TABLE
+// Mappings of key-value pairs
+// Extremely fast at insert, finding, and deleting values
+
+const hash = (key, size) => {
+  let hashedKey = 0;
+
+  for (let i = 0; i < key.length; i++) {
+    hashedKey = key.charCodeAt(i);
+  }
+
+  return hashedKey % size;
+}
+
+class HashTable {
+  constructor() {
+    this.size = 20;
+    this.buckets = Array(this.size);
+
+    for (let i = 0; i < this.buckets.length; i++) {
+      this.buckets[i] = new Map();
+    }
+  }
+
+  insert = (key, value) => {
+    let idx = hash(key, this.size);
+    this.buckets[idx].set(key, value);
+  }
+
+  remove = (key) => {
+    let idx = hash(key, this.size);
+    let deleted = this.buckets[idx].get(key);
+    this.buckets[idx].delete(key);
+    return deleted;
+  }
+
+  search = (key) => {
+    let idx = hash(key, this.size);
+    return this.buckets[idx].get(key);
+  }
+}
+
+
+const hashTable = new HashTable();
+hashTable.insert('lifeline', 'field medic');
+hashTable.insert('wraith', 'void walker');
+hashTable.insert('revenant', 'assassin robot');
+hashTable.insert('loba', 'thief');
+
+console.log(hashTable);
