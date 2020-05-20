d6dfd4436239518a5c6b64704c1ace2cbf682c57
arthurs14 <arthurcs426@gmail.com>
2020-05-19 18:03:22 -0700

    added all the methods that are used in this data structure

--- Binary-Search-Trees/BinarySearchTree.js
+++ Binary-Search-Trees/BinarySearchTree.js
@@ -2,4 +2,58 @@
 // Fast At: insertion, deletion, accessing any element,
 // naturally stay sorted
 
+class Node {
+  constructor () {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+}
 
+class BST {
+  constructor(value) {
+    this.root = new Node(value);
+    this.count = 0; // how many nodes are in the tree
+  }
+
+  size = () => {
+
+  }
+
+  insert = () => {
+
+  }
+
+  min = () => {
+
+  }
+
+  max = () => {
+
+  }
+
+  contains = () => {
+
+  }
+
+  // depth first search
+  // in-order
+  dfsInOrder = () => {
+
+  }
+
+  // pre-order
+  dfsPreOrder = () => {
+
+  }
+
+  // post-order
+  dfsPostOrder = () => {
+
+  }
+
+  // breadth first search
+  bfs = () => {
+
+  }
+}
