47bf61bc8c424caf4be7d68e2790f1ebdf3a5b28
arthurs14 <arthurcs426@gmail.com>
2020-05-27 16:59:43 -0700

    priority queue outline and explanation

---
+++ Queues/Priority-Queue/PriorityQueues.js
@@ -0,0 +1,28 @@
+// Priority Queue
+// 1. Each element of the priority queue has an property associated with it
+// 2. Elements are added to the queue as per the priority
+// 3. Lowest priority elements are removed first
+
+// Add elements to the queue according to the priority and remove them from the
+// front of the queue.
+
+// store element and its priority
+class EQlement {
+  constructor(element, priority) {
+    this.element = element;
+    this.priority = priority;
+  }
+}
+
+class PriorityQueue {
+  constructor() {
+    this.items = [];
+  }
+
+  // functions to be implemented
+  // enqueue(item priority)
+  // dequeue()
+  // front()
+  // isEmpty()
+  // printPQueue()
+}
