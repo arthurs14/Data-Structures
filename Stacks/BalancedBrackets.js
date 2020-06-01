cff2b947c1b70e93dc07f14ba95bec776ba213a3
arthurs14 <arthurcs426@gmail.com>
2020-06-01 14:57:56 -0700

    finished balanced brackets example

--- Stacks/BalancedBrackets.js
+++ Stacks/BalancedBrackets.js
@@ -13,16 +13,26 @@ const close = {
 const hasBalancedBrackets = (str) => {
   const stack = [];
 
-  for (const bracket in str) {
-    if(open[bracket]) {
+  // loop through string
+  for (const bracket of str) {
+    // if bracket exists in object add to stack
+    if (open.hasOwnProperty(bracket)) {
       stack.push(bracket);
-    } else if(close[bracket]) {
-      const ;
+    } else if (close.hasOwnProperty(bracket)) {
+      // pop off stack and check if corresponding close bracket matches
+      const openBracket = stack.pop();
+      const closeBracket = open[openBracket]
+
+      if (closeBracket !== bracket) {
+        return false;
+      }
     }
   }
+
+  return stack.length === 0;
 };
 
-hasBalancedBrackets('[][(){}'); // false
-hasBalancedBrackets('({)}'); // false
-hasBalancedBrackets('({[]})'); // true
-hasBalancedBrackets('text ( is allowed ){rwwrwrrww [] ()}'); // true
+console.log(hasBalancedBrackets('[][(){}')); // false
+console.log(hasBalancedBrackets('({)}')); // false
+console.log(hasBalancedBrackets('({[]})')); // true
+console.log(hasBalancedBrackets('text ( is allowed ){rwwrwrrww [] ()}')); // true
