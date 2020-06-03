1dca427258a01cfcaf51c2f3e5698f8e8e3832dc
arthurs14 <arthurcs426@gmail.com>
2020-06-03 15:24:11 -0700

    push and pop functions working

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -7,13 +7,27 @@ class Array {
   }
 
   // pushes new element to end of array
-  push = () => {
+  push = (element) => {
+    // get length to add element at said index
+    this.data[this.length] = element;
 
+    // increase length by 1
+    this.length++;
+    return this.data;
   }
 
   // takes element at the end of array
   pop = () => {
+    // set item to last element of array
+    const item = this.data[this.length - 1];
 
+    // delete item
+    delete this.data[this.length - 1];
+
+    // reduce length by 1
+    this.length--;
+
+    return this.data;
   }
 
   // inserts element at certain index
