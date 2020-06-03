10b6768024cb4fe9c0902ad24ee9534b33c6520e
arthurs14 <arthurcs426@gmail.com>
2020-06-03 15:20:52 -0700

    added function skeletons for array class

--- Arrays/Arrays.js
+++ Arrays/Arrays.js
@@ -5,4 +5,29 @@ class Array {
     // object to store elements
     this.data = {};
   }
+
+  // pushes new element to end of array
+  push = () => {
+
+  }
+
+  // takes element at the end of array
+  pop = () => {
+
+  }
+
+  // inserts element at certain index
+  insertAt = () => {
+
+  }
+
+  // deletes element at the certain index
+  deleteAt = () => {
+
+  }
+
+  // return element at index
+  getElementAtIndex = () => {
+
+  }
 }
