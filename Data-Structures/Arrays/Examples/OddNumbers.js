a83a4876e46203904eb153528334be497e2e2ec7
arthurs14 <arthurcs426@gmail.com>
2020-06-23 17:29:51 -0700

    add time and space complexity to arrays example

--- Data-Structures/Arrays/Examples/OddNumbers.js
+++ Data-Structures/Arrays/Examples/OddNumbers.js
@@ -2,5 +2,6 @@ const oddsOnly = (numbers) => {
   return numbers.filter(num => num % 2 !== 0);
 };
 
-
+// time complexity: O(n) -> due to creating a new array of certain size
+// space complexity: O(n) -> due to the size of the array changing
 console.log(oddsOnly([1,2,3,4,5,6,7])); // [1,3,5,7]
