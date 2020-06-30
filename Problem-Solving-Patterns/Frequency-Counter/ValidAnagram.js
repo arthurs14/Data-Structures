0a72621e6e06d7d2447a18f5dc1dd0e59b1c630f
arthurs14 <arthurcs426@gmail.com>
2020-06-29 18:01:54 -0700

    a solution using frequency counter

--- Problem-Solving-Patterns/Frequency-Counter/ValidAnagram.js
+++ Problem-Solving-Patterns/Frequency-Counter/ValidAnagram.js
@@ -6,9 +6,31 @@
 
 
 function validAnagram(str1, str2) {
-  if(str1.length !== str2.length) {
+  if (str1.length !== str2.length) {
     return false;
   }
+
+  // store strings into objects
+  let strCountOne = {};
+  let strCountTwo = {};
+
+  // loop through str1 and put into object
+  for (let char in str1) {
+    strCountOne[str1[char]] ? strCountOne[str1[char]]++ : strCountOne[str1[char]] = 1;
+  }
+
+  // loop through str2 and put into object
+  for (let char in str2) {
+    strCountTwo[str2[char]] ? strCountTwo[str2[char]]++ : strCountTwo[str2[char]] = 1;
+  }
+
+  // check if character exists in str2
+  for (let key in strCountOne) {
+    if (!(key in strCountTwo) && strCountOne[key] !== strCountTwo[key]) {
+      return false;
+    }
+  }
+
   return true;
 }
 
