dc48ff7175acf2fe58f48bc5cf95e505ce295af4
arthurs14 <arthurcs426@gmail.com>
2020-06-30 17:38:08 -0700

    create a for loop that will loop throughout the array to find unique values and increases count whenever it finds a one

--- Problem-Solving-Patterns/Multiple-Pointers/CountUniqueValues.js
+++ Problem-Solving-Patterns/Multiple-Pointers/CountUniqueValues.js
@@ -12,13 +12,24 @@ const countUniqueValues = (arr) => {
   // set a counter that adds when a new unique value is found
   let count = 0;
 
+  // set a temporary unique value to check
+  let value = 0;
+
   // Loop through arr to find unique values
   for (let i = 0; i < arr.length; i++) {
-
+    // if value does not equal the value in the array set it to that new value
+    // also increase count by 1 to indicate its a unique value
+    // keep checking throughout the array to see if there are other unique numbers
+    if (value !== arr[i]) {
+      value = arr[i];
+      count++;
+    }
   }
+
+  return count;
 };
 
-// console.log(countUniqueValues([1,1,1,1,1,2])); // 2
-// console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])); // 7
-// console.log(countUniqueValues([])); // 0
-// console.log(countUniqueValues([-2,-1,-1,0,1])); // 4
+console.log(countUniqueValues([1,1,1,1,1,2])); // 2
+console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])); // 7
+console.log(countUniqueValues([])); // 0
+console.log(countUniqueValues([-2,-1,-1,0,1])); // 4
