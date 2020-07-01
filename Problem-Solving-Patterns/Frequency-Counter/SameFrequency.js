dbc2fc87bd79878c1b5c9b85754f9279c9351231
arthurs14 <arthurcs426@gmail.com>
2020-07-01 16:27:08 -0700

    loop over one of the numbers into a frequency tablethen use it to check if the same amount of numbers exist in the second

--- Problem-Solving-Patterns/Frequency-Counter/SameFrequency.js
+++ Problem-Solving-Patterns/Frequency-Counter/SameFrequency.js
@@ -4,11 +4,30 @@
 * the following complexities: Time: O(N).
 */
 
-const sameFrequency = () => {
+const sameFrequency = (num1, num2) => {
+  // check to see if there are negative numbers and return false
+  if (num1 < 0 || num2 < 0) return false;
 
+  // convert numbers to strings to iterate over them
+  const num1ToString = num1.toString();
+  const num2ToString = num2.toString();
+
+  // create a frequency for one of the numbers;
+  let num1Frequency = {}
+
+  // loop over one string and fill object with frequencies
+  for (let i = 0; i < num1ToString.length; i++) {
+    num1Frequency[num1ToString[i]] ? num1Frequency[num1ToString[i]]++ : num1Frequency[num1ToString[i]] = 1;
+  }
+
+  console.log(num1Frequency);
 };
 
-console.log(sameFrequency(182, 281)); // true
-console.log(sameFrequency(34,14)); // false
-console.log(sameFrequency(3589578, 5879385)); // true
-console.log(sameFrequency(22,222)); // false
+sameFrequency(182, 281);
+// console.log(sameFrequency(182, 281)); // true
+// console.log(sameFrequency(34,14)); // false
+// console.log(sameFrequency(3589578, 5879385)); // true
+// console.log(sameFrequency(22,222)); // false
+
+// console.log(sameFrequency(-1, 3));
+// console.log(sameFrequency(1, -3));
