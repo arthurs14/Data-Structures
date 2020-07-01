55cc4bd4e31c0027b18ee35e1ee8ff30c3e2e2b2
arthurs14 <arthurcs426@gmail.com>
2020-06-30 17:32:47 -0700

    when array length is 0 it returns 0;

--- Problem-Solving-Patterns/Multiple-Pointers/CountUniqueValues.js
+++ Problem-Solving-Patterns/Multiple-Pointers/CountUniqueValues.js
@@ -5,6 +5,10 @@
 */
 
 const countUniqueValues = (arr) => {
+  // return 0 if arr has 0 vales
+  if (arr.length === 0) {
+    return 0;
+  }
   // set a counter that adds when a new unique value is found
   let count = 0;
 
@@ -14,7 +18,7 @@ const countUniqueValues = (arr) => {
   }
 };
 
-console.log(countUniqueValues([1,1,1,1,1,2])); // 2
-console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])); // 7
-console.log(countUniqueValues([])); // 0
-console.log(countUniqueValues([-2,-1,-1,0,1])); // 4
+// console.log(countUniqueValues([1,1,1,1,1,2])); // 2
+// console.log(countUniqueValues([1,2,3,4,4,4,7,7,12,12,13])); // 7
+// console.log(countUniqueValues([])); // 0
+// console.log(countUniqueValues([-2,-1,-1,0,1])); // 4
