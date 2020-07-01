33088793de5cfc107298036d5491c2f45fd81cc2
arthurs14 <arthurcs426@gmail.com>
2020-07-01 16:11:45 -0700

    sameFrequency exercise started

---
+++ Problem-Solving-Patterns/Frequency-Counter/SameFrequency.js
@@ -0,0 +1,9 @@
+/*
+* Write a function called sameFrequency. Given two positive integers, find out
+* if the two numbers have the same frequency of digits. Your solution MUST have
+* the following complexities: Time: O(N).
+*/
+
+const sameFrequency = () => {
+
+};
