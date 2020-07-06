228a9ecb6d278b17ca78c9b82a70b8c5cf97022b
arthurs14 <arthurcs426@gmail.com>
2020-07-06 16:51:46 -0700

    check if array is empty before starting any operation

--- Problem-Solving-Patterns/Multiple-Pointers/AveragePair.js
+++ Problem-Solving-Patterns/Multiple-Pointers/AveragePair.js
@@ -5,7 +5,12 @@
 * pair that matches the average target.
 */
 
-const averagePair = () => {};
+const averagePair = (arr, avg) => {
+  // if array empty return false
+  if (arr.length === 0) return false;
+
+
+};
 
 console.log(averagePair([1,2,3], 2.5)); // true
 console.log(averagePair([1,3,3,4,5,6,7,10,12,19], 8)); // true
