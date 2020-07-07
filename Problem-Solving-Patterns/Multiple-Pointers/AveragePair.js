5557e379423f5bc191d4f57b217296247e0f8597
arthurs14 <arthurcs426@gmail.com>
2020-07-06 17:29:25 -0700

    fixed solution to add all possible combinations rather than next consecutive values

--- Problem-Solving-Patterns/Multiple-Pointers/AveragePair.js
+++ Problem-Solving-Patterns/Multiple-Pointers/AveragePair.js
@@ -9,25 +9,36 @@ const averagePair = (arr, targetAvg) => {
   // if array empty return false
   if (arr.length === 0) return false;
 
-  // store average of two numbers
   let avg = 0;
 
-  // loop while array still has two values to add
-  while (arr.length >= 2) {
-    // find the average of two values
-    avg = (arr[0] + arr[1]) / 2;
+  // set let and right
+  let left = 0;
+  let right = arr.length - 1;
 
-    // remove first value
-    arr.shift();
+  // loop while left index is less than right index
+  while (left < right) {
+    // find the average of two values
+    console.log(`adding ${arr[left]} + ${arr[right]}`);
+    avg = (arr[left] + arr[right]) / 2;
 
     // if avg matches targetAvg then we can return true
-    if (avg === targetAvg) return true;
+    if (avg === targetAvg) {
+      return true;
+    }
+    // if avg is lower then  move left up one
+    else if (avg < targetAvg) {
+      left++;
+    }
+    // if avg is higher then move right down one
+    else {
+      right--;
+    }
   }
 
   return false;
 };
 
-console.log(averagePair([1,2,3], 2.5)); // true
-console.log(averagePair([1,3,3,4,5,6,7,10,12,19], 8)); // false
-console.log(averagePair([-1,0,3,4,5,6], 4.1)); // false
-console.log(averagePair([], 4)); // false
+// console.log(averagePair([1,2,3], 2.5)); // true
+console.log(averagePair([1,3,3,4,5,6,7,10,12,19], 8)); // true
+// console.log(averagePair([-1,0,3,4,5,6], 4.1)); // false
+// console.log(averagePair([], 4)); // false
