78311bfdc7270701c17dbb514fa289039433b189
arthurs14 <arthurcs426@gmail.com>
2020-07-06 17:00:46 -0700

    using a while loop to determine if two numbers in the array match the target average by removing the first index value to check the next two consecutive numbers for target average

--- Problem-Solving-Patterns/Multiple-Pointers/AveragePair.js
+++ Problem-Solving-Patterns/Multiple-Pointers/AveragePair.js
@@ -5,14 +5,27 @@
 * pair that matches the average target.
 */
 
-const averagePair = (arr, avg) => {
+const averagePair = (arr, targetAvg) => {
   // if array empty return false
   if (arr.length === 0) return false;
 
+  // store average of two numbers
+  let avg = 0;
 
+  // loop while array still has two values to add
+  while (arr.length >= 2) {
+    // find the average of two values
+    avg = (arr[0] + arr[1]) / 2;
+    // remove first value
+    arr.shift();
+    // if avg matches targetAvg then we can return true
+    if (avg === targetAvg) return true;
+  }
+
+  return false;
 };
 
-console.log(averagePair([1,2,3], 2.5)); // true
-console.log(averagePair([1,3,3,4,5,6,7,10,12,19], 8)); // true
+// console.log(averagePair([1,2,3], 2.5)); // true
+// console.log(averagePair([1,3,3,4,5,6,7,10,12,19], 8)); // false
 console.log(averagePair([-1,0,3,4,5,6], 4.1)); // false
-console.log(averagePair([], 4)); // false
+// console.log(averagePair([], 4)); // false
