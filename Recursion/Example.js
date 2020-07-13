ee4a89f9e732b7864503fd084cf4bc41b57cbf57
arthurs14 <arthurcs426@gmail.com>
2020-07-13 15:12:09 -0700

    a simple recursion function that counts down

--- Recursion/Example.js
+++ Recursion/Example.js
@@ -2,3 +2,15 @@
 // 1. Base Case -> what stops the recursion
 // 2. Different Input -> call the same function over and over, different data
 
+const countDown = num => {
+  if (num <= 0) {
+    console.log('All Done!');
+    return;
+  }
+
+  console.log(num);
+  num--;
+  countDown(num);
+};
+
+countDown(5);
