d01f81c9a6a6ab3d7166cab9dbb0486ab2d063bc
arthurs14 <arthurcs426@gmail.com>
2020-07-13 15:47:24 -0700

    added some tips for pure recursive functions

--- Recursion/Example.js
+++ Recursion/Example.js
@@ -2,6 +2,14 @@
 // 1. Base Case -> what stops the recursion
 // 2. Different Input -> call the same function over and over, different data
 
+/*
+* Pure Recursin Tips:
+* arrays -> use slice, the spread operator, and concat to not mutate arrays
+* strings are immutable -> slice, substr, or substring
+* copies of objects -> Object.assign, spread operator
+*/
+
+
 // simple coutdown function
 const countDown = num => {
   if (num <= 0) {
