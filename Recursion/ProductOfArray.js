3ad1142496c6d78f75376927b93cab65dcec08b7
arthurs14 <arthurcs426@gmail.com>
2020-07-14 15:52:17 -0700

    solution tto finding the product of array of numbers is working

--- Recursion/ProductOfArray.js
+++ Recursion/ProductOfArray.js
@@ -4,8 +4,8 @@
 */
 
 const productOfArray = arr => {
-  if (arr.length === 0) return 0;
-  // return arr[0] * productOfArray(arr.slice(1));
+  if (arr.length === 0) return 1;
+  return arr[0] * productOfArray(arr.slice(1));
 };
 
 console.log(productOfArray([1,2,3])); // 6
