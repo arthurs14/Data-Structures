8072a79f55f6958ac63beee9a1b9097aee024a34
arthurs14 <arthurcs426@gmail.com>
2020-07-15 15:22:06 -0700

    create a ssimple linear search function

---
+++ Algorithms/Search/LinearSearch.js
@@ -0,0 +1,13 @@
+/*
+* Create a function that accepts an array and a value. Check to see if the value
+* exists in the array and return the index of that value. If it does not exist
+* return -1
+*/
+
+const LinearSearch = (arr, value) => {
+
+};
+
+console.log(LinearSearch([1,2,3,4], 3)); // index: 2
+console.log(LinearSearch([1,3,5,7,9], 7)); // index: 3
+console.log(LinearSearch([2,4,6,8,10], 3)); // -1
