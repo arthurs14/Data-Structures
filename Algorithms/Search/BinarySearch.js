f9ed5f09a9853d210c000b8ebccb3a8144e68c70
arthurs14 <arthurcs426@gmail.com>
2020-07-15 16:04:50 -0700

    completed binary search with test cases

--- Algorithms/Search/BinarySearch.js
+++ Algorithms/Search/BinarySearch.js
@@ -4,7 +4,20 @@
 */
 
 const binarySearch = (arr, val) => {
+  let left = 0;
+  let right = arr.length - 1;
+  let center = Math.floor((left + right) / 2);
 
+  while (arr[center] !== val && left <= right) {
+    if (val < arr[center]) {
+      right = center - 1;
+    } else {
+      left = center + 1;
+    }
+    center = Math.floor((left + right) / 2);
+  }
+
+  return arr[center] === val ? middle : -1;
 };
 
 console.log(binarySearch([1,2,3,4,5], 2)); // 1
