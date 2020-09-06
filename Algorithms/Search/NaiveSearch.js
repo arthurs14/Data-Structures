4de43957d8904b08a4ddaadab5db729b505fee9d
Arthur Sandro <arthurcs426@gmail.com>
2020-09-06 16:28:00 -0700

    solution complete for substring search

--- Algorithms/Search/NaiveSearch.js
+++ Algorithms/Search/NaiveSearch.js
@@ -4,19 +4,21 @@
 */
 
 const naiveSearch = (long, short) => {
+  // count how many time substring appears in string
+  let count = 0;
+
+  // loop over long string
   for (let i = 0; i < long.length; i++) {
     for (let j = 0; j < short.length; j++) {
-      console.log(long[i], short[j]);
-      if (short[j] !== long[i + j]) {
-        console.log('BREAK!');
-        break;
-      }
+      // continue looping over substring if it corresponds with long string
+      if (short[j] !== long[i + j]) break;
 
+      // increase count if substring matches corresponding string
+      if (short.length - 1 === j) count++;
     }
   }
+  return count;
 }
 
-
-naiveSearch('lorie loled', 'lol');
-
-// console.log(naiveSearch('lorie loled', 'lol'));
+console.log(naiveSearch('lorie loled', 'lol'));
+console.log(naiveSearch('lorie loled', 'lo'));
