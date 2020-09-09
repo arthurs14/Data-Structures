71aa7d38bf13c82e65b328cc5e41170b795e3650
Arthur Sandro <arthurcs426@gmail.com>
2020-09-09 16:50:07 -0700

    setting variables and while loop for checking one array onto another

--- Algorithms/Intermediate-Sort/Merge-Sort/MergeSort.js
+++ Algorithms/Intermediate-Sort/Merge-Sort/MergeSort.js
@@ -6,3 +6,25 @@
 * Works by decomposing array into small arrays of 0 or 1 elements,
 * then building up a newly sorted array
 */
+
+// Merge two arrays into one sorted array (least to greatest)
+const MergeArrays = (arr1, arr2) => {
+  let arr = [];
+  let arrLength = 0;
+  let arrCount = 0;
+
+  while (arrLength !== arr1.length - 1) {
+    if (arr1[arrLength] < arr2[arrLength]) {
+      arr.push(arr1[arrLength]);
+      arrLength++;
+    } else {
+
+    }
+
+
+  }
+
+  return arr;
+}
+
+console.log(MergeArrays([1, 10, 50], [2, 14, 99, 100]));
